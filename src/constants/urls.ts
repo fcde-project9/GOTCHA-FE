d7467a3a4d04a672d7c16b869a7254c2a8211218
soojjung <89066676+soojjung@users.noreply.github.com>
2026-01-14 15:07:45 +0900

    feat: 우리 인스타 한번 볼래요?

---
+++ src/constants/urls.ts
@@ -0,0 +1,10 @@
+/**
+ * 외부 URL 상수
+ * 하드코딩 방지 및 중앙 관리를 위한 상수 파일
+ */
+
+// 소셜 미디어 URL
+export const SOCIAL_URLS = {
+  /** 공식 인스타그램 계정 */
+  INSTAGRAM: "https://www.instagram.com/gotcha_map",
+} as const;
